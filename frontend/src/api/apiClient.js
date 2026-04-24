0d9f60fec358d60504cd48db49f884ad0128a47c
unknown <iranzihugues18@gmail.com>
2026-04-24 01:11:43 -0700

    new 1update frontend

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://e-learning-i67o.onrender.com",
     headers: {
         Accept: "application/json"
     }
