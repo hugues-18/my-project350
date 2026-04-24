e8f96793e4823cb8a7f4f966e9708387541ec48d
unknown <iranzihugues18@gmail.com>
2026-04-24 01:54:23 -0700

    reupdate2

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 
 const dbpool = mysql.createPool({
   host: "198.251.89.126",
-  user: "isivgxsa_modules_user",
+  user: "isivgxsa_module_user",
   password: "=Y]A@ALM7d&_%McB",
   database: "isivgxsa_modules",
   port: 3306,
